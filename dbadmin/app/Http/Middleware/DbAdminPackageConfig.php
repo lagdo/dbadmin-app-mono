08cce50713710d04ef25970666626be45f25bc90
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-02-18 02:12:31 +0100

    Upgrade to the 0.31.2 release of the DbAdmin package.

--- dbadmin/app/Http/Middleware/DbAdminPackageConfig.php
+++ dbadmin/app/Http/Middleware/DbAdminPackageConfig.php
@@ -34,7 +34,7 @@ class DbAdminPackageConfig
                 ],
             ],
             'jaxon.lib.core.request.uri' => route(name: 'dbadmin.jaxon', absolute: false),
-            'jaxon.app.assets.file' => 'admin-0.10.0',
+            'jaxon.app.assets.file' => 'admin-0.10.1',
         ]);
 
         return $next($request);
--- dbadmin/app/Http/Middleware/DbAuditPackageConfig.php
+++ dbadmin/app/Http/Middleware/DbAuditPackageConfig.php
@@ -33,7 +33,7 @@ class DbAuditPackageConfig
                 ],
             ],
             'jaxon.lib.core.request.uri' => route(name: 'dbaudit.jaxon', absolute: false),
-            'jaxon.app.assets.file' => 'audit-0.10.0',
+            'jaxon.app.assets.file' => 'audit-0.10.1',
         ]);
 
         return $next($request);
